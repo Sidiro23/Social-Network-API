0a2a126976ebb9d97528a4a124e8865d19ae6820
soutsos <106550353+Sidiro23@users.noreply.github.com>
2022-08-19 19:17:54 -0500

    added index file to routes

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+//Import all API's from api/index.js
+const apiRoutes = require('./api');
+
+router.use('./api', apiRoutes);
+
+router.use((req, res)=> res.send('Wrong route!'));
+
+
+module.exports= router;
